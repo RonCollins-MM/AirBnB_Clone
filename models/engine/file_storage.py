0786b9be7b38305907f026de17d5a6eca56b2e5b
Roncollins MM <muriukiron@gmail.com>
2022-10-14 10:49:24 +0300

    Fixed bug where objects were dynamically being stored as dict instead of instances

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@ This module contains implementation for FileStorage class that is responsible
 to storage of objects to files and reading objects from files.
 """
 
+from models.base_model import BaseModel
 import json
 import os
 
@@ -19,7 +20,7 @@ class FileStorage():
     """
 
     __file_path = 'objects.json'
-    __objects = dict()
+    __objects = {}
 
     def all(self):
         """
@@ -31,31 +32,46 @@ class FileStorage():
 
     def new(self, obj):
         """
-        Sets in __objects the passed object with the key <class name>.id and
-        value of the object as a dictionary representation.
+        Sets in __objects the passed object with key as ``<class name>.id`` and
+        value as instance object of its respective class.
 
         Args:
             obj: The object to be stored
         """
 
         self.__objects.update({f'{obj.__class__.__name__}.{obj.id}' : \
-                          obj.to_dict()})
+                          obj})
 
     def save(self):
         """
         Serializes __objects to JSON file at the path specified in __file_path
-        attribute
+        attribute.
+
+        A local copy of ``__objects`` is created and then each object is converted
+        to its dictionary representation before being dumped into the JSON file.
         """
 
+        lcl_copy = self.__objects
+        obj_as_dict = {key: lcl_copy[key].to_dict() for key in lcl_copy.keys()}
         with open(self.__file_path, 'w', encoding='utf-8') as f:
-            f.write(json.dumps(self.__objects))
+            f.write(json.dumps(obj_as_dict))
 
     def reload(self):
         """
-        Deserializes JSON file to ``__objects``. If the file doesn't exist,
-        nothing happens.
+        Deserializes JSON file to ``__objects``.
+
+        All dictionary representation of objects are first converted to objects
+        instances of their respective classes before being added to the
+        ``__objects`` attribute.
+        If the file doesn't exist, nothing happens.
         """
 
         if os.path.exists(self.__file_path):
             with open (self.__file_path, 'r', encoding='utf-8') as f:
-                self.__objects = json.loads(f.read())
+                obj_as_dict = json.loads(f.read())
+                for v in obj_as_dict.values():
+                    #Class name is obtained from the dictionary then
+                    #class constructor called to create an object.
+                    #The object is then passed to self.new() to append object
+                    #to __objects attribute
+                    self.new(eval(v['__class__'])(**v))
